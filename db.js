ac66b8c086c22c1189f1bae26cff903aa0001bb2
Luis Rios <aluisrios10@gmail.com>
2024-11-13 21:20:36 -0800

    New

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/partyWithBrendiiis');  // Adjust your DB URI if needed
+    await mongoose.connect('mongodb://localhost:3000/partyWithBrendiiis');  // Adjust your DB URI if needed
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('Failed to connect to MongoDB', err);
